6694b1276066a6426d81d2f22124e034e37f829c
Akash T Rathod <104497742+akashrathod3565@users.noreply.github.com>
2026-02-03 20:42:29 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Akash Rathod",
+    author_email="akashrathod3565@gmail.com",
+    packages=find_packages()
+)
