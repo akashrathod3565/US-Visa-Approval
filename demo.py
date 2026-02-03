7972588e18c3761cef85c5939e35f67901bb4ad2
Akash T Rathod <104497742+akashrathod3565@users.noreply.github.com>
2026-02-03 22:17:46 +0530

    utils added

---
+++ demo.py
@@ -0,0 +1,13 @@
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
+
+# logging.info("Demo file is runnning") 
+
+
+
+try:
+    a = 2/0
+except Exception as e:
+    raise USvisaException(e, sys)
+
